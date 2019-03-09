a344e932f172029eacd5155463431960266d3e47
dazzu <navalkevin0@gmail.com>
2019-03-08 22:54:35 -0700

    Updated index.js
    - Added timestamps to messages

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ io.on('connection', function(socket){
     console.log('user disconnected');
   });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-	io.emit('chat message', msg);
+    let d = new Date();
+    let n = d.toLocaleTimeString();
+    console.log('message: ' + "[" + n + "] : " + msg);
+	  io.emit('chat message', "[" + n + "] : " + msg);
   });
 });
 
